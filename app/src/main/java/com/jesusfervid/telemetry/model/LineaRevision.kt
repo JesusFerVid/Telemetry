a17b3e582e3f69120c95c93d51e434d2cbaf2b84
Jesús Fernández Vidal <jesusfervid94@gmail.com>
2023-06-06 15:38:07 +0200

    refactor: Menos código
    Revision y LineaRevision han sido acortados.
    Poniendo la clave primaria como último atributo, podemos usar argumentos por defecto y ahorrar mucho código repetido.

--- app/src/main/java/com/jesusfervid/telemetry/model/LineaRevision.kt
+++ app/src/main/java/com/jesusfervid/telemetry/model/LineaRevision.kt
@@ -1,28 +1,31 @@
 package com.jesusfervid.telemetry.model
 
 import android.os.Parcelable
+import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import androidx.room.Relation
 import kotlinx.parcelize.Parcelize
 
 /** Representa cada línea del cuerpo de una revisión */
 @Entity(tableName = "linea_revision")
 @Parcelize
 data class LineaRevision(
-  @PrimaryKey(autoGenerate = true)
-  val id : Long?,
-
   val id_revision : Long,
   val tipo : String,
-  val realizada : Boolean,
-  val km : Int?
-) : Parcelable {
-  @Ignore
-  constructor(
-    tipo : String,
-    id_revision : Long,
-    realizada : Boolean = false,
-    km : Int? = null
-  ) : this(null, id_revision, tipo, realizada, km)
-}
+  val realizada : Boolean = false,
+  val km : Int? = null,
+  @PrimaryKey(autoGenerate = true) val id : Long? = null
+) : Parcelable
+
+/**
+ * Representa la relación 1:N entre Revision y LineaRevision
+ */
+data class RevisionConLineas (
+  @Embedded val revision : Revision,
+  @Relation(
+    parentColumn = "id",
+    entityColumn = "id_revision"
+  )
+  val lineasRevision : List<LineaRevision>
+)
--- app/src/main/java/com/jesusfervid/telemetry/model/Revision.kt
+++ app/src/main/java/com/jesusfervid/telemetry/model/Revision.kt
@@ -1,45 +1,22 @@
 package com.jesusfervid.telemetry.model
 
 import android.os.Parcelable
-import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
-import androidx.room.Relation
 import kotlinx.parcelize.Parcelize
 
-/** Representa una revision, de cualquier vehículo */
+/**
+ * Representa una revision, de cualquier vehículo.
+ * No es necesario pasar un id, se genera automáticamente
+ */
 @Entity(tableName = "revision")
 @Parcelize
 data class Revision(
-  @PrimaryKey(autoGenerate = true)
-  val id : Long?,
-
-  val id_vehiculo : Long?,
+  val id_vehiculo : Long,
   val fecha : String,
   val km : Int,
   val kmSiguiente : Int,
-  val observaciones: String?
-) : Parcelable {
-  // Constructor sin id. Lo generará Room.
-  @Ignore
-  constructor(
-    id_vehiculo : Long?,
-    fecha : String,
-    km : Int,
-    kmSiguiente : Int,
-    observaciones: String?
-  ) : this(null, id_vehiculo, fecha, km, kmSiguiente, observaciones)
-}
-
-data class RevisionConLineas (
-  @Embedded
-  val revision : Revision,
-
-  @Relation(
-    parentColumn = "id",
-    entityColumn = "id_vehiculo"
-  )
-  val lineasRevision : List<LineaRevision>
-)
+  val observaciones: String?,
+  @PrimaryKey(autoGenerate = true) val id : Long? = null
+) : Parcelable
 
