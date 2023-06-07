df14bace0288757e20706b01300b40d24ebc52f9
Jesús Fernández Vidal <jesusfervid94@gmail.com>
2023-06-07 18:28:40 +0200

    feat: Implementado el ViewModel para la lista de Vehículos.

---
+++ app/src/main/java/com/jesusfervid/telemetry/viewmodel/VehiculosViewModel.kt
@@ -0,0 +1,44 @@
+package com.jesusfervid.telemetry.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.jesusfervid.telemetry.model.Vehiculo
+import com.jesusfervid.telemetry.repository.VehiculoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel para "VehiculosFragment"
+ */
+class VehiculosViewModel(app : Application) : AndroidViewModel(app) {
+  val vehiculosLD = MutableLiveData<List<Vehiculo>>()
+
+  init {
+  	VehiculoRepository(getApplication<Application>().applicationContext)
+    getVehiculos()
+  }
+
+  // Estas funciones llaman al método homónimo del repositorio y actualizan el LiveData.
+  // Utilizan corrutinas, para no bloquear el hilo principal.
+  fun addVehiculo(vehiculo : Vehiculo) = viewModelScope.launch(Dispatchers.IO) {
+    VehiculoRepository.add(vehiculo)
+  }
+
+  fun addVehiculos(vehiculos : List<Vehiculo>) = viewModelScope.launch(Dispatchers.IO) {
+    VehiculoRepository.addAll(vehiculos)
+  }
+
+  fun modifyVehiculo(vehiculo : Vehiculo) = viewModelScope.launch(Dispatchers.IO) {
+    VehiculoRepository.modify(vehiculo)
+  }
+
+  fun removeVehiculo(vehiculo : Vehiculo) = viewModelScope.launch(Dispatchers.IO) {
+    VehiculoRepository.remove(vehiculo)
+  }
+
+  fun getVehiculos() = viewModelScope.launch(Dispatchers.IO) {
+    vehiculosLD.postValue(VehiculoRepository.getAll())
+  }
+}
