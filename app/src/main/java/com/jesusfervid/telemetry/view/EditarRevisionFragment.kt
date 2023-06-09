7298fe7c6aadbe0df97797718f01ba58ddeb2e63
Jesús Fernández Vidal <jesusfervid94@gmail.com>
2023-06-09 16:34:38 +0200

    feat: Añadidos fragments faltantes

---
+++ app/src/main/java/com/jesusfervid/telemetry/view/EditarRevisionFragment.kt
@@ -0,0 +1,40 @@
+package com.jesusfervid.telemetry.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jesusfervid.telemetry.databinding.FragmentEditarRevisionBinding
+
+
+/**
+ * En este [Fragment] podremos editar una revisión.
+ */
+class EditarRevisionFragment : Fragment() {
+  private var _binding : FragmentEditarRevisionBinding? = null
+
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater : LayoutInflater, container : ViewGroup?,
+    savedInstanceState : Bundle?
+  ) : View? {
+
+    _binding = FragmentEditarRevisionBinding.inflate(inflater, container, false)
+    return binding.root
+
+  }
+
+  override fun onViewCreated(view : View, savedInstanceState : Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+}
---
+++ app/src/main/java/com/jesusfervid/telemetry/view/RevisionesFragment.kt
@@ -0,0 +1,39 @@
+package com.jesusfervid.telemetry.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jesusfervid.telemetry.databinding.FragmentRevisionesBinding
+
+/**
+ * En este [Fragment] se muestra la lista de revisiones de un vehículo
+ */
+class RevisionesFragment : Fragment() {
+  private var _binding : FragmentRevisionesBinding? = null
+
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater : LayoutInflater, container : ViewGroup?,
+    savedInstanceState : Bundle?
+  ) : View? {
+
+    _binding = FragmentRevisionesBinding.inflate(inflater, container, false)
+    return binding.root
+
+  }
+
+  override fun onViewCreated(view : View, savedInstanceState : Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+}
